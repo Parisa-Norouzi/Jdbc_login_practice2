6949580d3a0ce97a505b059c1cb0de24179750e3
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-13 13:52:18 +0330

    add constrauctor, geter and seter, tostring in user class

--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,2 +1,59 @@
 public class Users {
+
+    private String FirstName;
+    private String LastName;
+    private String UserName ;
+    private String Password ;
+
+    public Users() {
+    }
+
+    public Users(String firstName, String lastName, String userName, String password) {
+        FirstName = firstName;
+        LastName = lastName;
+        UserName = userName;
+        Password = password;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "FirstName='" + FirstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", UserName='" + UserName + '\'' +
+                ", Password='" + Password + '\'' +
+                '}';
+    }
 }
