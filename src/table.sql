983017c9c4ab888211d36e21590bbbc6b9aa6dc3
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-13 13:37:29 +0330

    creat table user

---
+++ src/table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE if not exists Users (
+                          User_id serial primary key ,
+                          First_Name varchar(255),
+                          Last_Name varchar(255),
+                          UserName varchar (255)
+);
