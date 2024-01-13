d7a40b5de77b6010c2aede38772cb6118f52e10c
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-13 23:25:18 +0330

    add object in main

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Menu {
+    private final Scanner scanner=new Scanner(System.in);
+    public void  publicMenu(){
+
+        System.out.println("*****welcome*******");
+        System.out.println("1-sign up");
+        System.out.println("1-sign in");
+        System.out.println("1-exist");
+        int number =scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("number is "+number);
+    }
 }
