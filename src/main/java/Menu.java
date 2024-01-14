7cbd9027757dd44c9a433afd3ec912735d648bb9
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-14 12:49:17 +0330

    add constructor

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 
 public class Menu {
     private final Scanner scanner=new Scanner(System.in);
+    private final UserService userService=new UserService();
     public void  publicMenu(){
 
         System.out.println("*****welcome*******");
         System.out.println("1-sign up");
-        System.out.println("1-sign in");
-        System.out.println("1-exist");
+        System.out.println("2-sign in");
+        System.out.println("3-exist");
         int number =scanner.nextInt();
         scanner.nextLine();
-        System.out.println("number is "+number);
+        switch (number){
+            case 1 -> signUp();
+            case 2-> System.out.println("signUp");
+            case 3 -> System.out.println("exist");
+        }
+    }
+    public void signUp(){
+        userService.singUp();
     }
 }
--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class UserService {
+    private final Scanner scanner=new Scanner(System.in);
+    public void singUp(){
+        System.out.println("Please enter your id: ");
+        String id=scanner.nextLine();
+        System.out.println("Please enter your firstname: ");
+        String firstname=scanner.nextLine();
+        System.out.println("Please enter your lastname: ");
+        String lastname=scanner.nextLine();
+        System.out.println("Please enter your userName: ");
+        String userName=scanner.nextLine();
+        System.out.println("Please enter your passWord: ");
+        String Password=scanner.nextLine();
+        Users users=new Users();
+        System.out.println(users);
+    }
 }
